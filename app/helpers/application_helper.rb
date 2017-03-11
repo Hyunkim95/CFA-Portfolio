fc615acd195640261fac02538b4ad94f7bc83004
Hyun Kim <hkim@live.com.au>
2017-03-11 20:32:59 +1100

    First commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+    def is_active?(link_path)
+   if current_page?(link_path)
+     "active"
+   else
+     ""
+   end
+  end
+
+  def disable(link_path)
+   if current_page?(link_path)
+     "collapse"
+   else
+     ""
+   end
+ end
+
+ def open_menu(link_path)
+  if current_page?(link_path)
+    ""
+  else
+    "collapse"
+  end
+  end
+
 end
